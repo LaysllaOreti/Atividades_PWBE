c9d9880875adcc2d1cab6e4395c72da062aec53e
laysllaeduarda <laysllaoreti2006@gmail.com>
2025-08-15 15:28:03 -0300

    Desenvolvimento dia 15.08

---
+++ Aulas_POO/tamagoshi.py
@@ -0,0 +1,7 @@
+class Tamagoshi:
+    def __init__ (self, nome, fome, saude, idade, tedio):
+        self.nome = nome
+        self.fome = 0
+        self.saude = 100
+        self.idade = 0
+        self.tedio = 0
